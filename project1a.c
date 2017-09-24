483e53fdcf6eee7264955b5c8713307c61a0a641
Steven Milov <sjm141@pitt.edu>
2017-09-24 18:52:31 -0400

    finished the program. added recursion for looping

--- project1a.c
+++ project1a.c
@@ -7,31 +7,40 @@ void play(int*,int*);
 int getResult(char*);
 
 int main(){
-  char toPlay;
+  char toPlay[10];
   int player = 0;
   int computer = 0;
   int* playerLoc = &player;
   int* computerLoc = &computer;
   printf("Welcome to Rock, Paper, Scissors\n");
-  printf("\nWould you like to play? (y/n)");
-  scanf("%c",&toPlay);
+  do{
+    printf("\nWould you like to play? (yes/no) ");
+    scanf("%s",toPlay);
+  }while(strcmp(toPlay,"yes") != 0 && strcmp(toPlay,"no") != 0);
 
-
-  if(toPlay == 'y'){
+  if(strcmp(toPlay,"yes") == 0){
     play(playerLoc,computerLoc);
   }
-  else if(toPlay == 'n'){
+  else if(strcmp(toPlay,"no") == 0){
     printf("Fine then. Go play with someone else.\n");
   }
+  else
+    printf("This isn't possible.");
+
 
   return 0;
 }
 
 void play(int* playerLoc, int* computerLoc){
+
+  char playAgain[10];
   char choice[100]; //rock,paper,scissors
   int result; //0=tie, 1=userWon, 2=compWon
-  printf("\nWhat is your choice? ");
-  scanf("%s",choice);
+
+  do{
+    printf("\nWhat is your choice? ");
+    scanf("%s",choice);
+  }while(strcmp(choice,"rock") != 0 && strcmp(choice,"paper") != 0 && strcmp(choice,"scissors") != 0);
 
   result = getResult(choice);
   if(result == 0){
@@ -39,12 +48,39 @@ void play(int* playerLoc, int* computerLoc){
   }
   else if(result == 1){
     printf("You win this game!\n");
+    *playerLoc = *playerLoc +1;
   }
   else if(result == 2){
     printf("You lose this game!\n");
+    *computerLoc = *computerLoc+1;
   }
 
+  printf("\nThe score is now you: %d computer: %d\n",*playerLoc,*computerLoc);
 
+
+  if(*playerLoc == 3){
+    printf("You win this match!");
+    *playerLoc = 0;
+    *computerLoc = 0;
+    do{
+      printf("\nWould you like to play again? (yes/no) ");
+      scanf("%s",playAgain);
+    }while(strcmp(playAgain,"yes") != 0 && strcmp(playAgain,"no") != 0);
+  }
+  else if(*computerLoc == 3){
+    printf("\nThe computer wins this match!\n");
+    *computerLoc = 0;
+    *playerLoc = 0;
+    do{
+      printf("\nWould you like to play again? (yes/no) ");
+      scanf("%s",playAgain);
+    }while(strcmp(playAgain,"yes") != 0 && strcmp(playAgain,"no") != 0);
+  }
+  else{
+    play(playerLoc,computerLoc);
+  }
+  if(strcmp(playAgain,"yes") == 0)
+    play(playerLoc,computerLoc);
 }
 
 int getResult(char* c){
